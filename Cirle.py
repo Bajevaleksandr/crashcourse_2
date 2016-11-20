30867d6d67e648987f2a905afec428f7dc316116
Bajevaleksandr <bajevaleksandr@gmail.com>
2016-11-20 18:56:11 +0100

    lesson 2 commit

--- Cirle.py
+++ Cirle.py
@@ -7,7 +7,19 @@ def circle ():
     pt = Point(100, 50)
     pt.draw(win)
     cir = Circle(pt, field_draw)
+
+    print ("Nazovi chislo, suchechka!")
+    number26=int(input())
+    print("xAxA")
+    print("Nazovi esche odno chislo, suchechka!")
+    number27 = int(input())
+    cir.setOutline('black')
+    color=color_rgb(number26, 0, number27)
+    cir.setFill(color)
     cir.draw(win)
+    # for i in range(46):
+    #     circle().move(5, 0)
+    #     time.sleep(.05)
 circle()
 
 input ("Press enter to continue")
