23445fed0d6a19e49d4366601947d8b8858e4f29
Bajevaleksandr <bajevaleksandr@gmail.com>
2016-11-20 18:21:28 +0100

    lesson 2 commit

--- Lesson N2.py
+++ Lesson N2.py
@@ -1,7 +1,22 @@
 def chiotnoenechiotnoe ():
     print ("Nazovi chislo, suchechka!")
     number26=int(input())
-    if number26%2==0: print ("chiotnoe")
-    else: print ("nechiotnoe")
-    print ("xaxa")
+    # if number26%2==0: print ("chiotnoe")
+    # else: print ("nechiotnoe")
+    print ("xAxA")
+    print("Nazovi esche odno chislo, suchechka!")
+    number27 = int(input())
+    print ("Vvedi odin iz etich symvolov: * / + -")
+    action=input()
+    if action=="*":
+        print (number26*number27)
+    if action == "/":
+        print(number26 / number27)
+    if action == "+":
+        print(number26 + number27)
+    if action == "-":
+        print(number26 - number27)
+    else: print("Delai chto tebe skazali, ueban!!!")
+
 chiotnoenechiotnoe()
+
