a428d6f15f110692c38699c7eac797b01d6dc81f
Bajevaleksandr <bajevaleksandr@gmail.com>
2016-11-20 18:02:58 +0100

    lesson 2 commit

---
+++ Lesson N2.py
@@ -0,0 +1,7 @@
+def chiotnoenechiotnoe ():
+    print ("Nazovi chislo, suchechka!")
+    number26=int(input())
+    if number26%2==0: print ("chiotnoe")
+    else: print ("nechiotnoe")
+
+chiotnoenechiotnoe()
